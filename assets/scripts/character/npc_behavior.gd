f8d1b427495743c901f7b5be5eb7a766f2a09f03
Max Seavey <58485607+cvmaxie@users.noreply.github.com>
2023-04-01 23:35:21 -0400

    updated shlorping functionality

--- assets/scripts/character/npc_behavior.gd
+++ assets/scripts/character/npc_behavior.gd
@@ -2,6 +2,7 @@ class_name NPCBehavior
 extends Interactive
 
 export (NodePath) var _shlorping_target_path : NodePath
+export (NodePath) var _physical_collider_path : NodePath
 export var _knot_name := "Abstract"
 export var _final_knot_name := "Abstract"
 export var _override_set_monitoring : bool
@@ -11,20 +12,12 @@ var _shlorping_out = false
 
 
 onready var _shlorping_target : Sprite = get_node(_shlorping_target_path)
+onready var _physical_collider : CollisionShape2D = get_node(_physical_collider_path)
 
 
 
 func _ready():
-	print(_shlorping_target)
-	if Globals.DialogueBox._ink_player.VisitCountAtPathString(_final_knot_name) > 0:
-		self.set_monitoring(false)
-		_shlorping_target.set_visible(false)
-		return
-	
-	if not _override_set_monitoring:
-		self.set_monitoring(true)
-	
-	_shlorping_target.set_visible(true)
+	set_monitoring_ready()
 
 
 func _process(_delta):
@@ -42,6 +35,21 @@ func _on_ActiveArea_cannot_interact() -> void:
 
 func _get_object_name() -> String:
 	return _knot_name
+
+
+func set_monitoring_ready():
+	if Globals.DialogueBox._ink_player.VisitCountAtPathString(_final_knot_name) > 0:
+		self.set_monitoring(false)
+		_shlorping_target.set_visible(false)
+		_physical_collider.set_deferred("disabled", true)
+		return
+	
+	if not _override_set_monitoring:
+		self.set_monitoring(true)
+		_physical_collider.set_deferred("disabled", false)
+	
+	_shlorping_target.set_visible(true)
+	_physical_collider.set_deferred("disabled", false)
 
 
 # Appear from the void (visually)
@@ -69,15 +77,16 @@ func shlorp():
 			_shlorping_target.set_visible(false)
 			_shlorping_out = false
 			_shlorping_target.material.set_shader_param("time", 0)
+			_physical_collider.set_deferred("disabled", true)
 			
 			return
-			
 			
 	elif _shlorping_in:
 		_shlorping_target.material.set_shader_param("time", timeValue + .05)
 		if timeValue > 1.56:
 			_shlorping_in = false
 			_shlorping_target.material.set_shader_param("time", 1.57)
+			_physical_collider.set_deferred("disabled", false)
 			
 			return
 
--- assets/scripts/character/rinas_brain.gd
+++ assets/scripts/character/rinas_brain.gd
@@ -1,4 +1,4 @@
-extends Interactive
+extends NPCBehavior
 
 export var rina_positions : Dictionary = {
 	"VOID": Vector2(-1000, -1000),
@@ -12,27 +12,16 @@ export var rina_positions : Dictionary = {
 var current_room_index = 1
 var current_position = "VOID"
 var next_room_index = 3 # store the next room for whenever the player is actually in it
-var _shlorping_in = false
-var _shlorping_out = false
 var _next_position
 
-onready var rina_sprite = self.get_parent().get_child(0)
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	Globals.Rina = self
-#	if RoomEngine.CurrentRoom == RoomEngine.Rooms[1]:
-#		move_rina("START")
-#	rina_shlorp_in()
 	# SAVE SYSTEM: Keep track of rina's current state in ink
 	self.get_parent().set_global_position(rina_positions.get(current_position))
-
-
-func _process(_delta):
-	if Input.is_action_just_pressed("party_leader_switch"):
-		print("RINA'S COORDINATES: " + String(self.get_parent().get_global_position()))
-
-	if _shlorping_in or _shlorping_out:
-		rina_shlorp()
+	
+	set_monitoring_ready()
 
 
 # change states (starting knot name)
@@ -68,14 +57,11 @@ func move_rina(newPosition):
 	current_position = newPosition
 		 
 	if RoomEngine.CurrentRoomIndex == current_room_index: # we are in the same room
-		rina_shlorp_out()
-		
-		#rina_shlorp_in()
+		shlorp_out()
 
 	elif RoomEngine.CurrentRoomIndex == newPositionRoomIndex:
-		#rina_shlorp_out(true, rina_positions.get(newPosition))
 		place_rina_in_new_room()
-		rina_shlorp_in()
+		shlorp_in()
 
 
 # call this only when the player is actually in the room
@@ -85,44 +71,39 @@ func place_rina_in_new_room():
 	RoomEngine.move_object_to_new_room(self.get_parent(), RoomEngine.Rooms[current_room_index], RoomEngine.Rooms[next_room_index])
 	self.get_parent().set_global_position(rina_positions.get(current_position))
 	current_room_index = next_room_index
-	rina_shlorp_in()
+	shlorp_in()
 
 
-# appear from the void (visually)
-func rina_shlorp_in():
-	rina_sprite.material.set_shader_param("time", 0.0)
-	_shlorping_in = true
-	get_parent().set_pause_mode(false)
-
-
-func rina_shlorp():
-	var timeValue = rina_sprite.material.get_shader_param("time")
+func shlorp():
+	var timeValue = _shlorping_target.material.get_shader_param("time")
 	
 	if _shlorping_out:
-		rina_sprite.material.set_shader_param("time", timeValue - .05)
+		_shlorping_target.material.set_shader_param("time", timeValue - .05)
 		if timeValue < .05:
 			_shlorping_out = false
-			rina_sprite.material.set_shader_param("time", 0)
-			self.get_parent().set_global_position(RoomEngine.CurrentRoom.room_bounds_min)
+			_shlorping_target.material.set_shader_param("time", 0)
+			self.set_monitoring(false)
+			_physical_collider.set_deferred("disabled", true)
 			
 			if current_room_index == next_room_index:
 				self.get_parent().set_global_position(rina_positions.get(current_position))
 				_shlorping_in = true
-				#get_parent().set_pause_mode(true)
 				
 			return
 			
 			
 	elif _shlorping_in:
-		rina_sprite.material.set_shader_param("time", timeValue + .05)
+		_shlorping_target.material.set_shader_param("time", timeValue + .05)
 		if timeValue > 1.56:
 			_shlorping_in = false
-			rina_sprite.material.set_shader_param("time", 1.57)
+			_shlorping_target.material.set_shader_param("time", 1.57)
+			self.set_monitoring(true)
+			_physical_collider.set_deferred("disabled", false)
 			
 			return
 
 
 # disappear into the void (visually)
-func rina_shlorp_out():
+func shlorp_out():
 	_shlorping_out = true
 
