4233ba17c70b2630281ed68864cf5bce90d84058
Max <ms12047@nyu.edu>
2023-04-12 10:37:39 -0400

    map pin precision upgrade

--- assets/scripts/ui/map_manager.gd
+++ assets/scripts/ui/map_manager.gd
@@ -1,10 +1,12 @@
 extends Control
 
 export var map_pin : NodePath
-export var hallway_walkable_top_left := Vector2(-136, -360)
+export var hallway_walkable_top_left := Vector2(-144, -376)
 export var hallway_walkable_bottom_right := Vector2(1072, 832)
 export var map_walkable_top_left := Vector2(168, 104)
 export var map_walkable_bottom_right := Vector2(568, 432)
+export var bandn_pin_pos := Vector2(338, 343)
+export var topicspot_pin_pos := Vector2(517, 82)
 
 var party_position := Vector2(0, 0)
 
@@ -29,8 +31,8 @@ func toggle_map() -> void:
 		return
 		
 	self.set_visible(true)
-	# if in hallway
-	if RoomEngine.CurrentRoomIndex == 1:
+	
+	if RoomEngine.CurrentRoom == RoomEngine.hallway: # HALLWAY
 		var roomPosLerpX = inverse_lerp(hallway_walkable_top_left.x, hallway_walkable_bottom_right.x, Globals.Nour.get_global_position().x)
 		var roomPosLerpY = inverse_lerp(hallway_walkable_top_left.y, hallway_walkable_bottom_right.y, Globals.Nour.get_global_position().y)
 		
@@ -38,3 +40,8 @@ func toggle_map() -> void:
 		var newPinPosY = lerp(map_walkable_top_left.y, map_walkable_bottom_right.y, roomPosLerpY)
 		pin.set_global_position(Vector2(newPinPosX, newPinPosY))
 		
+	elif RoomEngine.CurrentRoom == RoomEngine.bandn: # BANDN
+		pin.set_global_position(bandn_pin_pos)
+		
+	elif RoomEngine.CurrentRoom == RoomEngine.topicspot: # TOPICSPOT
+		pin.set_global_position(topicspot_pin_pos)
