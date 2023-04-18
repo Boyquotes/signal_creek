78b086916a4b53a4d5ca086a06d080100401aaa0
Max Seavey <58485607+cvmaxie@users.noreply.github.com>
2023-04-17 21:14:57 -0400

    shader modifications do not pull yet

--- assets/scripts/character/npc_behavior.gd
+++ assets/scripts/character/npc_behavior.gd
@@ -2,17 +2,16 @@ class_name NPCBehavior
 extends Interactive
 
 export (NodePath) var _shlorping_target_path : NodePath
+export var _shlorp_animation_name : String
 export (NodePath) var _physical_collider_path : NodePath
 export var _knot_name := "Abstract"
 export var _final_knot_name := "Abstract"
 export var _override_set_monitoring : bool
 export var visibility := true
 
-var _shlorping_in = false
-var _shlorping_out = false
 var _physical_collider
 
-onready var _shlorping_target : Sprite = get_node(_shlorping_target_path)
+onready var _shlorping_target : AnimationPlayer = get_node(_shlorping_target_path)
 
 
 
@@ -23,9 +22,9 @@ func _ready():
 	set_monitoring_ready()
 
 
-func _process(_delta):
-	if _shlorping_in or _shlorping_out:
-		shlorp()
+#func _process(_delta):
+#	if _shlorping_in or _shlorping_out:
+#		shlorp()
 
 
 func _on_ActiveArea_can_interact() -> void:
@@ -55,7 +54,7 @@ func set_monitoring_ready():
 		if _physical_collider:
 			_physical_collider.set_deferred("disabled", false)
 	
-	_shlorping_target.set_visible(visibility)
+#	_shlorping_target.set_visible(visibility)
 	
 	if _physical_collider:
 		_physical_collider.set_deferred("disabled", false)
@@ -63,12 +62,10 @@ func set_monitoring_ready():
 
 # Appear from the void (visually)
 func shlorp_in():
-	print(_shlorping_target)
-	print("shlorping in")
-	_shlorping_target.set_visible(true)
-	_shlorping_target.material.set_shader_param("progress", 1.0)
-	_shlorping_in = true
-	visibility = true
+	if _physical_collider:
+		_physical_collider.set_deferred("disabled", false)
+		
+	_shlorping_target.play(_shlorp_animation_name)
 	
 	# For the position of the center, get the positoin of self relative to camera
 	var camPosWithOffset = Vector2(self.get_global_position().x + Globals.DialogueBox.camera_offset_dialogue, self.get_global_position().y)
@@ -81,10 +78,11 @@ func shlorp_in():
 
 # Disappear into the void (visually)
 func shlorp_out() -> void:
-	print(_shlorping_target)
-	print("shlorping out")
-	_shlorping_out = true
-	visibility = false
+	_shlorping_target.play(_shlorp_animation_name)
+	
+	if _physical_collider:
+		_physical_collider.set_deferred("disabled", true)
+			
 	
 	var camPosWithOffset = Vector2(self.get_global_position().x + Globals.DialogueBox.camera_offset_dialogue, self.get_global_position().y)
 	
@@ -100,32 +98,3 @@ func shlorp_out() -> void:
 	
 	if not _override_set_monitoring:
 		self.set_monitoring(false)
-
-
-func shlorp():
-	var timeValue = _shlorping_target.material.get_shader_param("progress")
-	
-	if _shlorping_out:
-		_shlorping_target.material.set_shader_param("progress", timeValue + .05)
-		if timeValue > .95:
-			_shlorping_target.set_visible(false)
-			_shlorping_out = false
-			_shlorping_target.material.set_shader_param("progress", 1.0)
-			
-			if _physical_collider:
-				_physical_collider.set_deferred("disabled", true)
-			
-			return
-			
-	elif _shlorping_in:
-		_shlorping_target.material.set_shader_param("progress", timeValue - .05)
-		if timeValue < 0.05:
-			_shlorping_in = false
-			_shlorping_target.material.set_shader_param("progress", 0)
-			
-			if _physical_collider:
-				_physical_collider.set_deferred("disabled", false)
-			
-			return
-
-
